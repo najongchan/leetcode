917d06ee0b4efe970234cb974126ea9060c1d82d
JongChan Na <sirume92@godo.co.kr>
2019-10-04 10:45:11 +0900

    #2

---
+++ week3#2.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/next-permutation/
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
